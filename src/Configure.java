624c1764f5266fda7401182dcb7c35e7eee13b65
Lettuceman44 <sdneamtu@gmail.com>
2013-11-02 14:12:27 -0400

    Configure.java now holds the name, patter, and holds a reference to CollectionActor.
    Configure.java is now finished

--- src/Configure.java
+++ src/Configure.java
@@ -1,9 +1,20 @@
 import akka.actor.ActorRef;
 
+/**
+ * 
+ * @author Stefan Neamtu
+ * 
+ * Class that holds the file name, the pattern, and the CollectionActor.
+ * An immutable object that is sent to ScanActors so they know what they are scanning for.
+ * It sends a message to CollectionActor when it has found something.
+ *
+ */
 public class Configure {
-	private String name;
-	private String pattern;
-	private ActorRef cActor;
+	
+	private final String name;
+	private final String pattern;
+	private final ActorRef cActor;
+	
 	public Configure(String name, String pattern, ActorRef cActor){
 		this.name = name;
 		this.pattern = pattern;
