6be9ec1b1c57c6a6fea4c4358f2c51cb4ef23a62
SidIcarus <app6141@gmail.com>
2013-11-02 16:55:35 -0400

    Added a stub functions and comments for them

--- src/CollectionActor.java
+++ src/CollectionActor.java
@@ -1,7 +1,12 @@
 import akka.actor.UntypedActor;
 
 /**
- * 
+ * The CollectionActor receives two types of messages. The first, of class FileCount (which should be received 
+ * exactly once), contains a count of the number of files being scanned. The format of this immutable message 
+ * object are up to you. The remaining messages are Found objects, which, upon receipt, are printed by the 
+ * CollectionActor. Printout consists of the file name (or "-" for standard input) and the list of matching lines.
+ *  When all the Found messages have been processed, the CollectionActor shuts down all actors using
+ *   Actors.registry().shutdown() - see page 186 in PCJVM for an example.
  * @author Group 6
  */
 public class CollectionActor extends UntypedActor {
--- src/ScanActor.java
+++ src/ScanActor.java
@@ -12,24 +12,66 @@ import java.util.regex.Pattern;
 
 /**
  * ScanActor is used to scan a given file for a given pattern
- * 
+*
+ * A ScanActor expects to receive exactly one immutable Configure message containing 
+ * (a) a String with the name of the file to scan (or null for the standard input), and 
+ * (b) an ActorRef to a CollectionActor, which collects and prints scan results. 
+ * 		The actual format of the Configure objects is up to you.
+ * Each ScanActor will construct an immutable Found object containing a String with the name of the file, 
+ * 		and a List<String> with one entry in the list for each matching line. Each string in the list 
+ * 		consists of the line number, a space, and the text of the line itself. The list must be ordered by 
+ * 		location of the line in the file (i.e., first matching line at position 0, second matching line at 
+ * 		position 1, etc.). The Found object is sent as the one and only message from the ScanActor to the 
+ * 		CollectionActor.
  * @author Group 6
  */
 public class ScanActor extends UntypedActor {
-
+	private String fileName;
+	private String patternString;
+	private CollectionActor cActor;
+	private Pattern pattern;
+	
 	/*
 	 * Unused Constructor
 	 */
 	public ScanActor() {}
 	
+	/*
+	 * (non-Javadoc)
+	 * @see akka.actor.UntypedActor#onReceive(java.lang.Object)
+	 */
 	@Override
 	public void onReceive(Object arg0) throws Exception {
+		if (arg0.getClass() == Configure.class){
+			this.fileName = "";
+					
+			this.pattern = Pattern.compile(this.patternString);
+		}
+	}
+	
+	/*
+	 * Creates a BufferedReader for stdin and gives it to ScanActor#read() 
+	 * to find pattern matches, then sends the Found message to the given 
+	 * CollectionActor.
+	 */
+	private void scanStdIn(){
 		
 	}
 	
 	/*
-	 * 
-	 * 
+     * Creates a BufferedReader for the given file and gives it to 
+     * ScanActor.read() to find pattern matches in the file. Then, it 
+     * sends the results in a Found message to the CollectionActor.
+	 */
+	private void scanFile(){
+		
+	}
+	
+	/*
+     * Reads using the given BufferedReader and checks for 
+     * pattern matches line by line, storing them in an ArrayList.
+     * Strings in the list have the format: "<line number> <line text>".
+     * 
 	 * @param buffRead The BufferedReader sued for reading.
 	 * @return A list of the text's lines with their number.
 	 */
